b507b423e502bcf7e4d2b797ee312879d0ad9f99
Pyxis <pyxis@uzuki.live>
2018-07-02 21:11:49 +0900

    implement SocialLoginTest

---
+++ sociallogintest/src/main/java/com/github/windsekirun/sociallogintest/MainApplication.kt
@@ -0,0 +1,35 @@
+package com.github.windsekirun.sociallogintest
+
+import android.app.Application
+
+import pyxis.uzuki.live.sociallogin.SocialLogin
+import pyxis.uzuki.live.sociallogin.impl.SocialType
+import pyxis.uzuki.live.sociallogin.kakao.KakaoConfig
+
+/**
+ * SocialLogin
+ * Class: MainApplication
+ * Created by Pyxis on 7/2/18.
+ *
+ *
+ * Description:
+ */
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        SocialLogin.init(this)
+        val kakaoConfig = KakaoConfig.Builder()
+                .setRequireEmail()
+                .setRequireNickname()
+                .setRequireAgeRange()
+                .setRequireBirthday()
+                .setRequireEmail()
+                .setRequireGender()
+                .setRequireImage()
+                .build()
+
+        SocialLogin.addType(SocialType.KAKAO, kakaoConfig)
+    }
+}
